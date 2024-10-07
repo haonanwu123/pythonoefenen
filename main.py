ec71ea38999515166f2da65d2dc6b337ca7fc689
haonanwu123 <wuhaonan99@gmail.com>
2024-10-07 11:08:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -71,3 +71,55 @@
 
 # password = generate_password(length)
 # print("Password: " + password)
+
+import getpass
+
+def choose_word():
+    word = getpass.getpass("Player 1, please enter a word:") # No effect in environments like Jupyter Notebook
+    return word.lower()
+
+def display_word(word, guessed_letters):
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter
+        else:
+            display += "_"
+    return display
+
+def Galgje():
+    print("Welcome to the word guessing game!")
+
+    word_to_guess = choose_word()
+    guessed_letters = []
+    incorrect_guesses = 0
+
+    while True:
+        print("\nword:", display_word(word_to_guess, guessed_letters))
+        guess = input("Player 2, please guess a letter:").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a valid letter.")
+            continue
+
+        if guess in guessed_letters:
+            print("You have already guessed this letter, please try other letters.")
+            continue
+
+        guessed_letters.append(guess)
+
+        if set(guessed_letters) == set(word_to_guess):
+            print("Congratulations! You guessed the entire word. The correct word is:", word_to_guess)
+            break
+        elif guess in word_to_guess:
+            print("Guessed it!")
+        else:
+            print("Guessed wrong!")
+            incorrect_guesses += 1
+
+        if incorrect_guesses == 9:
+            print("Sorry, you have guessed wrong 9 times in a row. The game is over. The correct word is:", word_to_guess)
+            break
+
+if __name__ == "__main__":
+    Galgje()
